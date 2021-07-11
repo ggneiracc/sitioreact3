8f5bad0066d7b7cda8057ab47b5d5569b8170662
ggneiracc68 <ggneiracc68@gmail.com>
2021-07-11 15:15:17 -0500

    Versión 5 de sitio React con Bootstrap

--- src/App.js
+++ src/App.js
@@ -11,22 +11,44 @@ import Matriz from "./components/C08matriz";
 
 function App() {
   return (
-    <div>
-      <C01componente />
-      <br/>
-      <C02contador />
-      <br/>
-      <C03componenteConCss />
-      <br/>
-      <EjemploDeEstado />
-      <br/>
-      <EjemploDeDobleEstado />
-      <br/>
-      <ComponenteConVariable xVariable="Hola mundo... desde... React" />
-      <br/>
-      <OperadorTernario xEdad = "20" />
-      <br/>
-      <Matriz />
+    <div className="container">
+          
+      <header class="row bg-secondary">
+        Header (Cabecera)
+        <C01componente />
+      </header>
+
+      <nav class="row bg-success">
+        nav (Navegador-menu)
+      </nav>
+
+      <section class="row bg-primary"> 
+        <article class="col-md-6 bg-warning"> 
+          Articulo 1
+          <Matriz />
+        </article>
+        
+        <article class="col-md-4 bg-secondary">
+          Articulo 2
+          <EjemploDeEstado />
+          <br/>
+          <EjemploDeDobleEstado />
+          <br/>
+          <OperadorTernario xEdad = "20" />
+          <br/>
+          <ComponenteConVariable xVariable="Hola mundo... desde... React" />
+        </article>
+
+        <aside class="col-md-2 bg-primary">
+          aside (Apartado)
+          <C03componenteConCss />
+        </aside>
+      </section>
+
+      <footer class="row bg-dark text-light">
+        footer (Pie de página)
+        <C02contador />
+      </footer>
     </div>
   );
 }
