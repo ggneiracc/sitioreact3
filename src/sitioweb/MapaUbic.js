fb73c19bba35ee37725c443c932ae4f2a085f4bf
ggneiracc68 <ggneiracc68@gmail.com>
2021-07-30 00:13:01 -0500

    Versión 21. iframe con title 30jul2021 12:12 pm

--- src/sitioweb/MapaUbic.js
+++ src/sitioweb/MapaUbic.js
@@ -4,7 +4,7 @@ const MapaUbic = () => {
     return (
         <Fragment>
             <div className="d-flex align-items-stretch pt-2">
-                <iframe id="video" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827.7331251796195!2d-71.55089728735355!3d-16.38755174218354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424a6a0172d35d%3A0xebeb3b98aa2420d9!2sInstituto%20Superior%20Honorio%20Delgado%20Espinoza!5e0!3m2!1ses-419!2spe!4v1626164448236!5m2!1ses-419!2spe" width="100%" height="320" style={{border:0}} allowfullscreen="" loading="lazy"></iframe>
+                <iframe id="video" title="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827.7331251796195!2d-71.55089728735355!3d-16.38755174218354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424a6a0172d35d%3A0xebeb3b98aa2420d9!2sInstituto%20Superior%20Honorio%20Delgado%20Espinoza!5e0!3m2!1ses-419!2spe!4v1626164448236!5m2!1ses-419!2spe" width="100%" height="320" style={{border:0}} allowfullscreen="" loading="lazy"></iframe>
             </div>
             <h5>Los Arcess 202. Distrito de Cayma - Arequipa.</h5> <h6>Número de contacto: 054232323</h6>
         </Fragment>
